11c298d6b60a47626ea0ee6022e79c1417f7ffe0
mayur bhoi <mayurbhoi200@gmail.com>
2022-08-24 11:03:09 +0530

    add useState

--- src/components/App.js
+++ src/components/App.js
@@ -28,10 +28,18 @@ function App() {
   /**
    * fetch data from api on mount. 
    */
+  
+    useEffect(async () => {
+    const responce = await fetch("https://jsonplaceholder.typicode.com/users");
+    let Data1 = await responce.json();
+    
+    setData(Data1);
+    // console.log(data);
+  }, []);
 
   return (
   <div className='App'>
-    { data.map((_data, i) => <p key={_data.id}>{i}.&nbsp;{_data.name}</p>) }
+    { data.map((_data, i) => <p key={_data.id}>{i+1}.&nbsp;{_data.name}</p>) }
   </div>
   ); 
 }
